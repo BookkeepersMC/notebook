9adb913b32bf80f953188f34adefd3ac7cb76e6c
CrypticVerse <crypticversemc@gmail.com>
2024-08-07 18:52:37 -0400

    Kotlin DSL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,23 +2,18 @@ pluginManagement {
 	repositories {
 		gradlePluginPortal()
 		mavenCentral()
-		maven {
-			url = "https://bookkeepersmc.github.io/m2/"
-		}
+		maven("https://bookkeepersmc.github.io/m2/")
 
-		maven {
-			url = "https://maven.fabricmc.net/"
-		}
+		maven("https://maven.fabricmc.net/")
 	}
 }
 
-rootProject.name = 'notebook-api'
+rootProject.name = "notebook-api"
 
 include("notebook-api-base")
 include("notebook-common-tags-v1")
 include("notebook-datagen-api-v1")
 include("notebook-keybind-api-v1")
-include("notebook-kotlin-adapter-v1")
 include("notebook-lifecycle-events-v1")
 include("notebook-mods-screen-v0")
 include("notebook-networking-api-v1")
